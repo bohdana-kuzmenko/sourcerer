d876ad0d3653cb7ad28370f7dcc6e16ec35ea1fb
Bohdana Kuzmenko <bohdana_kuzmenko@apple.com>
2023-01-03 21:53:32 +0100

    Cleanup(UI): decomposite settings page

--- sourcerer-ui/src/containers/settings/index.tsx
+++ sourcerer-ui/src/containers/settings/index.tsx
@@ -7,6 +7,28 @@ import {useNavigate} from "react-router-dom";
 import {SettingsApi} from "../../api/settings-api";
 
 
+interface InputFieldParams {
+    condition: boolean;
+    onChange: Function;
+    label: string,
+    placeholder: string
+}
+
+const InputField = (props: InputFieldParams) => {
+    if (!props.condition) {
+        return null
+    }
+    return (
+        <Form.Field
+            onChange={ props.onChange }>
+            <label>{ props.label }</label>
+            <input placeholder={ props.placeholder }/>
+        </Form.Field>
+    )
+
+
+}
+
 const selectRegisteredCredentials = (state: any) => state.registeredCredentials
 export default function SettingsPage() {
     const dispatch = useDispatch();
@@ -42,15 +64,15 @@ export default function SettingsPage() {
 
     return (
         <>
-            <Menu fixed='top' inverted style={{backgroundColor: '#011627'}}>
+            <Menu fixed='top' inverted style={ {backgroundColor: '#011627'} }>
                 <Container>
-                    <Menu.Item as='a' onClick={() => navigate('/storages')}>Home</Menu.Item>
+                    <Menu.Item as='a' onClick={ () => navigate('/storages') }>Home</Menu.Item>
                     <Menu.Item position='right'> <AuthStatus/></Menu.Item>
                 </Container>
             </Menu>
-            <Grid style={{height: '100%', padding: '14px', paddingTop: '70px'}}>
+            <Grid style={ {height: '100%', padding: '14px', paddingTop: '70px'} }>
                 <Grid.Column textAlign='right'>
-                    <Button color={"yellow"} labelPosition='left' icon onClick={() => setOpen(true)}>
+                    <Button color={ "yellow" } labelPosition='left' icon onClick={ () => setOpen(true) }>
                         <Icon name='plus'/> Add Credentials
                     </Button>
                     <Table>
@@ -65,10 +87,11 @@ export default function SettingsPage() {
                                 registeredCredentials.map((creds: any) => {
                                     return (
                                         <Table.Row>
-                                            <Table.Cell collapsing><Checkbox slider checked={creds.active}/></Table.Cell>
-                                            <Table.Cell>{creds.provider}</Table.Cell>
-                                            <Table.Cell>{creds.credentials}</Table.Cell>
-                                            <Table.Cell>{creds.created_at}</Table.Cell>
+                                            <Table.Cell collapsing><Checkbox slider
+                                                                             checked={ creds.active }/></Table.Cell>
+                                            <Table.Cell>{ creds.provider }</Table.Cell>
+                                            <Table.Cell>{ creds.credentials }</Table.Cell>
+                                            <Table.Cell>{ creds.created_at }</Table.Cell>
                                         </Table.Row>
                                     )
                                 })
@@ -78,81 +101,81 @@ export default function SettingsPage() {
                 </Grid.Column>
             </Grid>
             <Modal
-                onClose={() => {
+                onClose={ () => {
                     setProvider("")
                     setFormData({})
                     setOpen(false)
-                }}
-                onOpen={() => {
+                } }
+                onOpen={ () => {
                     setProvider("")
                     setFormData({})
                     setOpen(true)
-                }}
-                open={open}
+                } }
+                open={ open }
             >
                 <Modal.Header>Add sources credentials</Modal.Header>
                 <Modal.Content>
-                    <Form onSubmit={() => null} id={'credentials-form'}>
+                    <Form onSubmit={ () => null } id={ 'credentials-form' }>
                         <Form.Select
                             fluid
                             label='Provider'
-                            options={options}
+                            options={ options }
                             placeholder='Provider'
-                            onChange={(e: any, {value}) => {
+                            onChange={ (e: any, {value}) => {
                                 // @ts-ignore
                                 setProvider(value?.toString() || "")
-                            }}
+                            } }
                         />
-                        {S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 &&
-                            <Form.Field
-                                onChange={(event: any) => setFormData({
-                                    ...formData,
-                                    secret_access_key: event.target.value,
-                                })}>
-                                <label>AWS Secret Access Key</label>
-                                <input placeholder='AWS Secret Access Key'/>
-                            </Form.Field>}
-                        {S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 &&
-                            <Form.Field
-                                onChange={(event: any) => setFormData({
-                                    ...formData,
-                                    access_key: event.target.value,
-                                })}>
-                                <label>AWS Access Key</label>
-                                <input placeholder='AWS Access Key'/>
-                            </Form.Field>}
-                        {S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 &&
-                            <Form.Field
-                                onChange={(event: any) => setFormData({
-                                    ...formData,
-                                    endpoint_url: event.target.value,
-                                })}>
-                                <label>Endpoint url</label>
-                                <input placeholder='AWS Secret Access Key'/>
-                            </Form.Field>}
-                        {['MCQUEEN'].indexOf(provider) > -1 &&
-                            <Form.Field
-                                onChange={(event: any) => setFormData({
-                                    ...formData,
-                                    region: event.target.value,
-                                })}>
-                                <label>Region</label>
-                                <input placeholder='Region'/>
-                            </Form.Field>}
+                        <InputField
+                            condition={ S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 }
+                            onChange={ (event: any) => setFormData({
+                                ...formData,
+                                secret_access_key: event.target.value,
+                            }) }
+                            label={ "AWS Secret Access Key" }
+                            placeholder={ "AWS Secret Access Key" }
+                        />
+                        <InputField
+                            condition={ S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 }
+                            onChange={ (event: any) => setFormData({
+                                ...formData,
+                                access_key: event.target.value,
+                            }) }
+                            label={ "AWS Access Key" }
+                            placeholder={ "AWS Secret Access Key" }
+                        />
+                        <InputField
+                            condition={ S3_COMPATIBLE_BUCKETS.indexOf(provider) > -1 }
+                            onChange={ (event: any) => setFormData({
+                                ...formData,
+                                endpoint_url: event.target.value,
+                            }) }
+                            label={ "Endpoint url" }
+                            placeholder={ "Endpoint url" }
+                        />
+                        <InputField
+                            condition={ ["MCQUEEN"].indexOf(provider) > -1 }
+                            onChange={ (event: any) => setFormData({
+                                ...formData,
+                                region: event.target.value,
+                            }) }
+                            label={ "Region" }
+                            placeholder={ "Region" }
+                        />
                     </Form>
                 </Modal.Content>
                 <Modal.Actions>
-                    <Button onClick={() => {
+                    <Button onClick={ () => {
                         setProvider("")
                         setFormData({})
                         setOpen(false)
-                    }}>Cancel</Button>
-                    <Button onClick={() => {
+                    } }>Cancel</Button>
+                    <Button onClick={ () => {
                         send();
                         setProvider("")
                         setFormData({})
                         setOpen(false)
-                    }} positive>
+                    } } positive>
                         Ok
                     </Button>
                 </Modal.Actions>
