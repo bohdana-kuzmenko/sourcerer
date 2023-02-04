3b5bd9e9271a09fea45d98fe82b580e1288d8a3b
Bohdana Kuzmenko <bohdana_kuzmenko@apple.com>
2023-02-04 18:25:44 +0100

    Feature(UI): update setup

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
+    package_data={'': ['sourcerer/plugins/sourcerer_ui/*']},
     zip_safe=False,
     platforms='any',
     install_requires=[
