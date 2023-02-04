c0e2869c46dd0e5f1d9da2505a577058e41fcbf5
Bohdana Kuzmenko <bohdana_kuzmenko@apple.com>
2023-02-04 19:23:35 +0100

    Feature(UI): update login

--- sourcerer/cli.py
+++ sourcerer/cli.py
@@ -5,12 +5,22 @@ from argparse import ArgumentParser
 import uvicorn
 
 
-
-
 def run_ui_build():
     try:
-        subprocess.run("npm install", shell=True, cwd=os.path.join(pathlib.Path(__file__).parent.resolve(),"plugins/sourcerer_ui"))
-        subprocess.run("npm run build", shell=True, cwd=os.path.join(pathlib.Path(__file__).parent.resolve(),"plugins/sourcerer_ui"))
+        subprocess.run("npm install", shell=True,
+                       cwd=os.path.join(pathlib.Path(__file__).parent.resolve(), "plugins/sourcerer_ui"))
+        subprocess.run("npm run build", shell=True,
+                       cwd=os.path.join(pathlib.Path(__file__).parent.resolve(), "plugins/sourcerer_ui"))
+        from sourcerer.plugins.sourcerer_ui.fast_api_app_extended import app
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+    except Exception as ex:
+        data = None
+        raise ex
+    # return data
+
+
+def run_with_ui():
+    try:
         from sourcerer.plugins.sourcerer_ui.fast_api_app_extended import app
         uvicorn.run(app, host="0.0.0.0", port=8000)
     except Exception as ex:
@@ -23,14 +33,19 @@ def run_server():
     from sourcerer.frameworks.fastapi.app import app
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
+
 def main():
     parser = ArgumentParser(description='Sourcerer cli')
-    parser.add_argument('--action', choices=['run', 'build-ui'], default='run')
+    parser.add_argument('--action', choices=['run', 'run-with-ui', 'build-ui'], default='run')
     args = vars(parser.parse_args())
     print(args)
     if args.get('action') == 'run':
         run_server()
     if args.get('action') == 'build-ui':
         print(run_ui_build())
+    if args.get('action') == 'run-with-ui':
+        print(run_with_ui())
+
+
 if __name__ == "__main__":
     main()
